30ba2c3c2fd42266428db9270cfb4d2134cca755
Muabid <bruno2698@gmail.com>
2022-05-22 21:34:14 -0300

    Clean IngredientService

--- app/src/main/java/com/example/quecomohoy/data/services/ingredients/IngredientService.kt
+++ app/src/main/java/com/example/quecomohoy/data/services/ingredients/IngredientService.kt
@@ -1,7 +1,5 @@
 package com.example.quecomohoy.data.services.ingredients
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import com.example.quecomohoy.data.model.Ingredient
 import com.example.quecomohoy.data.services.RetrofitFactory
 import kotlinx.coroutines.Dispatchers
@@ -14,9 +12,7 @@ class IngredientService {
     suspend fun getIngredientsByName(name: String): List<Ingredient> {
         return withContext(Dispatchers.IO) {
             val response = retrofit.create(IngredientApiClient::class.java).getIngredientsByName(name)
-            val x = response.body()?: emptyList()
-            Log.i(TAG, x.size.toString())
-            x
+            response.body()?: emptyList()
         }
     }
 
