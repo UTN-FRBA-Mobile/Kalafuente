73e221a2615d836884e608b2b3c98681ff2f3d21
Iñaki Yabar Bilbao <inayabarb@gmail.com>
2022-06-05 14:42:15 -0300

    Image picker working

--- app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
+++ app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
@@ -1,26 +1,34 @@
 package com.example.quecomohoy.ui.scanIngredients
 
 import android.Manifest
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.startActivityForResult
-import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import com.example.quecomohoy.databinding.FragmentScanIngredientsBinding
+import java.io.File
+import java.io.IOException
+
 
 class ScanIngredientsFragment: Fragment() {
     private var _binding: FragmentScanIngredientsBinding? = null
     private val binding get() = _binding!!
 
     companion object {
-        private val IMAGE_CHOOSE = 1000
+        private val REQUEST_GALLERY_IMAGE = 100
+        private val REQUEST_PERMISSIONS = 13;
         private const val PICK_FROM_GALLERY = 1
     }
 
@@ -60,8 +68,33 @@ class ScanIngredientsFragment: Fragment() {
     }
 
     private fun chooseImageGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
-        startActivityForResult(requireActivity(), intent, IMAGE_CHOOSE, Bundle())
+        startActivityForResult(
+            Intent.createChooser(intent, "Select an image"), REQUEST_GALLERY_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_GALLERY_IMAGE && resultCode == Activity.RESULT_OK && data != null){
+            performCloudVisionRequest(data.getData());
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun performCloudVisionRequest(uri: Uri?) {
+        if (uri != null) {
+            try {
+                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(requireActivity().getApplicationContext().getContentResolver(), uri)
+                // callCloudVision(bitmap)
+                binding.selectedImage.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                Log.e(TAG, e.localizedMessage)
+            }
+        }
     }
 }
+
+private lateinit var filePhoto: File
+private const val FILE_NAME = "photo.jpg"
