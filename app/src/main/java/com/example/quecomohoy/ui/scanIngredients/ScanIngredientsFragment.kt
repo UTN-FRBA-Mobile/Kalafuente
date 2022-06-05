42252b0c4c349da442dcbd875cd1d57de85cfccf
Iñaki Yabar Bilbao <inayabarb@gmail.com>
2022-06-05 14:03:25 -0300

    Fix navigation to scan

--- app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
+++ app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
@@ -1,15 +1,20 @@
 package com.example.quecomohoy.ui.scanIngredients
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.quecomohoy.databinding.FragmentScanIngredientsBinding
 
 class ScanIngredientsFragment: Fragment() {
     private var _binding: FragmentScanIngredientsBinding? = null
     private val binding get() = _binding!!
+    private val PICK_FROM_GALLERY = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +25,11 @@ class ScanIngredientsFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.selectImageButton.setOnClickListener {  }
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED) {
+            binding.selectImageButton.setOnClickListener {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PICK_FROM_GALLERY)
+            }
+        }
     }
 }
