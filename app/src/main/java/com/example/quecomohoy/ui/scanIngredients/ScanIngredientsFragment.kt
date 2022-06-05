c5bd77bb7226b509975b8080e6d25a1000cc1cca
Iñaki Yabar Bilbao <inayabarb@gmail.com>
2022-06-05 14:19:13 -0300

    Gallery access permission

--- app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
+++ app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
@@ -1,20 +1,28 @@
 package com.example.quecomohoy.ui.scanIngredients
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import com.example.quecomohoy.databinding.FragmentScanIngredientsBinding
 
 class ScanIngredientsFragment: Fragment() {
     private var _binding: FragmentScanIngredientsBinding? = null
     private val binding get() = _binding!!
-    private val PICK_FROM_GALLERY = 1
+
+    companion object {
+        private val IMAGE_CHOOSE = 1000
+        private const val PICK_FROM_GALLERY = 1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +33,35 @@ class ScanIngredientsFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) !=
-            PackageManager.PERMISSION_GRANTED) {
             binding.selectImageButton.setOnClickListener {
-                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PICK_FROM_GALLERY)
+                if (checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PICK_FROM_GALLERY)
+                } else{
+                    chooseImageGallery();
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PICK_FROM_GALLERY -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    chooseImageGallery()
+                }else{
+                    Toast.makeText(requireContext(),"Permiso de camara denegado", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+    }
+
+    private fun chooseImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(requireActivity(), intent, IMAGE_CHOOSE, Bundle())
     }
 }
