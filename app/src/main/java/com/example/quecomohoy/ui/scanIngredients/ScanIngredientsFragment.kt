067d6495664f55914c4b68f01d8ee07808841a98
Iñaki Yabar Bilbao <inayabarb@gmail.com>
2022-06-05 13:40:39 -0300

    Fragment wip

--- app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
+++ app/src/main/java/com/example/quecomohoy/ui/scanIngredients/ScanIngredientsFragment.kt
@@ -1,6 +1,25 @@
 package com.example.quecomohoy.ui.scanIngredients
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.quecomohoy.databinding.FragmentScanIngredientsBinding
 
 class ScanIngredientsFragment: Fragment() {
+    private var _binding: FragmentScanIngredientsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentScanIngredientsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.selectImageButton.setOnClickListener {  }
+    }
 }
